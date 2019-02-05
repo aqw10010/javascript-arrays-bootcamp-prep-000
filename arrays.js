71384b26bfcb8695621963cc8fd098485b699e81
Austin Williams <austinq.williams@gmail.com>
2019-02-05 22:58:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ console.log(entrepreneurs[2] + bio);
 entrepreneurs[9];
 
 function addElementToBeginningOfArray(array, element) {
-    entrepreneurs.unshift(0)
+    array.unshift(0)
     return entrepreneurs.unshift(0)
     
 }

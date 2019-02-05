bca693946097e9aeaea34c59b4b2b23d7f0eee0b
Austin Williams <austinq.williams@gmail.com>
2019-02-05 22:26:41 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = 
+["snickers", "hundred grand", 
+"kit kat", "skittles" ] ;
+var entrepreneurs = ["Oprah Winfrey", "Laurene Powell Jobs", "Arianna Huffington"];
+ 
+console.log(entrepreneurs[0]);
+ 
+var bio = " is the co-founder and editress-in-chief of The Huffington Post";
+console.log(entrepreneurs[2] + bio);
+ 
+entrepreneurs[9];
+
+function addElementToBeginningOfArray(array, element) {
+    chocolateBars.unshift(element)
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  
+  return array
+
+}
+
+function addElementToEndOfArray(array, element) {
+    chocolateBars.push(element)
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+  
+}
